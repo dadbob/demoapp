27ede1e04296eb6751be2570c8e6fed32dc67855
dadbob <nuno.gomes@faurecia-aptoide.com>
2025-11-14 14:23:21 +0000

    Initial Import

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in!')}} <a href="/demo">Check the demo</a>
                 </div>
             </div>
         </div>
